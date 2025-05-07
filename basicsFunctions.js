40119cfb52718eb685f0fe3d1782b6e78c432c74
jasnael94 <jasnael94@gmail.com>
2025-05-07 14:46:39 +0200

    first basic functions

---
+++ basicsFunctions.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ */
+function sum(a, b) {
+    return a+b
+    }
+    
+ // sample usage - do not modify
+console.log(sum(1, 3));
+console.log(sum(2, 5));
+
+
+
+
+function multiply(a, b) {
+        return a*b
+     }
+     
+     // sample usage - do not modify
+     console.log(multiply(2, 4));
+     console.log(multiply(3, 2));

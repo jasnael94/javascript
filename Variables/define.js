ae8407e60b89ba6d03c5e18de2f508dc31e439cb
jasnael94 <jasnael94@gmail.com>
2025-05-12 17:14:28 +0200

    new cahoter "Variable"

---
+++ Variables/define.js
@@ -0,0 +1,12 @@
+// Define a variable 
+
+// Define a variable called count with an original value of 0 and then increment it (add 1 to it) on the following line.
+
+
+
+//define a variable "count" with value 0
+let count=0;
+
+
+//then increment it
+count += 1 ;

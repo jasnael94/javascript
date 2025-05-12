22c2e6aea9244848c823f289f84de046294f2fd6
jasnael94 <jasnael94@gmail.com>
2025-05-12 16:38:34 +0200

    new chapter "Number"

---
+++ Number/nbtoString.js
@@ -0,0 +1,21 @@
+
+// NUMBER TO STRING
+
+// Complete the function convertNumberToString such that it converts the number it receives into a string.
+// /**
+
+
+//  * @param {number} number
+//  */
+
+
+function convertNumberToString(number) {
+    return number.toString()
+    }
+    
+    // Sample usage - do not modify
+    console.log(convertNumberToString(42)); // "42"
+    console.log(convertNumberToString(97)); // "97"
+    
+    
+   
---
+++ Number/parseInt/age.js
@@ -0,0 +1,9 @@
+
+// @param {string} age
+
+
+
+export function getNextAge(age) {
+    return parseInt(age,10)+1
+    }
+    

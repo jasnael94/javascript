7eaf31478684b10e4ddd9783e4226f01e9a5cae1
jasnael94 <jasnael94@gmail.com>
2025-05-12 13:06:55 +0200

    string practices done

---
+++ string/capitalize.js
@@ -0,0 +1,20 @@
+//
+// CAPITALIZE WORD : 
+
+// Complete the function capitalize such that it capitalizes the word parameter it receives.
+//  There's no capitalize method in JavaScript, so you have to write it yourself.
+
+// Look at the sample usage to understand what capitalization means, then look at the hints if it's still not clear.
+// 
+
+// @param {string} word
+ 
+function capitalize(word) {
+    return word[0].toUpperCase()+word.substring(1).toLowerCase()
+    
+    }
+    
+    // Sample usage - do not modify
+    console.log(capitalize("sam")); // "Sam"
+    console.log(capitalize("ALEX")); // "Alex"
+    console.log(capitalize("chARLie")); // "Charlie"
---
+++ string/table2/instrucions.md
@@ -0,0 +1,11 @@
+
+
+# Name Variation Project
+
+In this project, you will practice your newly acquired string skills!
+
+Complete the given functions as described in the comments and try the app that you just built in the BROWSER tab.
+
+Take your time, make as many mistakes as possible and learn from them!
+
+---

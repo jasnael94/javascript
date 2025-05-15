c2e147e31892367ccb83d8b23b7d9e0c95a94ad1
jasnael94 <jasnael94@gmail.com>
2025-05-15 18:27:09 +0200

    New chapter "Array"

---
+++ Arrays/emptyArray.js
@@ -0,0 +1,14 @@
+// Empty array 
+
+// Complete the function getEmptyArray such that it returns an empty array.
+
+
+/**
+ * @return {[]} 
+ */
+function getEmptyArray() {
+    return []
+}
+
+// Sample usage - do not modify
+console.log(getEmptyArray());
